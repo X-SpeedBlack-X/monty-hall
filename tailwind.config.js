fe170f7385ddfb306bbd89858edc60b06066a1ea
Isaque <isaque.error404@gmail.com>
2022-08-18 12:15:30 -0300

    feat: add news colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          400: "#5c7e32",
+          500: "#7aa944",
+        },
+        gray: {
+          300: "#555",
+        },
+        background: {
+          100: "#444",
+        },
+      },
+    },
   },
   plugins: [],
 };
