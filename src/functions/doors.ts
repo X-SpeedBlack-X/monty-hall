0da81cebcf2401aef8edcc031c69c4b38f436376
Isaque <isaque.error404@gmail.com>
2022-08-19 11:49:16 -0300

    feat(functions): create functions

---
+++ src/functions/doors.ts
@@ -0,0 +1,11 @@
+// import DoorHooks from "../hooks/door";
+
+// export function createDoors(numberDoors: number, doorGift: number) {
+//   return Array.from({length: numberDoors}, (_,i) => {
+//     const number = i + 1;
+//     const haveGift = number === doorGift
+//     return new DoorHooks(number, haveGift)
+//   }
+
+//   )
+// }
